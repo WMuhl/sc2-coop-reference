edb5eed06cb79789f70b00ee0ff261d68f4d6b1c
Waldemar Muhl <waldemarmuhl@gmail.com>
2024-11-12 18:36:26 +0000

    Update GitHub link for SC2 Co-op Reference
    Changed the GitHub link from WMuhl to waltiplayer for repository submissions. This ensures users are directed to the correct page for submitting additions or corrections.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
             <h1>Starcraft 2 Co-op Reference</h1>
             <small>Big thanks to <a href="https://starcraft2coop.com/">https://starcraft2coop.com/</a> for all the awesome resources, this is for reference only. Check out their site for more in-depth information.</small>
             <br/>
-            <small>Built using Reactjs and PicoCSS. Head to the <a href="https://github.com/WMuhl/sc2-coop-reference">GitHub page</a> if you want to submit additions or corrections.</small>
+            <small>Built using Reactjs and PicoCSS. Head to the <a href="https://github.com/waltiplayer/sc2-coop-reference">GitHub page</a> if you want to submit additions or corrections.</small>
             <hr/>
             <div className="grid">
                 <CommanderList/>
